f3d654465e458b952ed0d9da8aba5a2f8157430d
Felix Muzny <muzny@colorado.edu>
2018-10-23 12:16:16 -0600

    add code files for pe 10

--- programming_exercises/pe10_code/Society.h
+++ programming_exercises/pe10_code/Society.h
@@ -7,15 +7,28 @@ class City {
 public:
     City(int const pop, std::string const name) :
     population_(pop),
-    name_(name)
+    name_(name),
+    percent_change_(0.0)
     {}
 
     int get_population() const { return population_; };
     std::string get_name() const { return name_; };
 
+    void Grow(int const num_people);
+
+    void Shrink(int const num_people);
+
+    bool IsBig() const;
+
+    void set_percent_change(double const percent) { percent_change_ = percent; };
+
 private:
     int population_;
     std::string name_;
+
+    double percent_change_;
+
+    friend std::ostream& operator<<(std::ostream &os, const City &c);
 };
 
 
@@ -24,8 +37,15 @@ public:
     
     void AddCity(int id, int population, std::string name) { cities_[id] = new City(population, name); };
 
+
+    void GrowCities();
+    
+
+
 private:
     std::map<int, City *> cities_;
+
+    friend std::ostream& operator<<(std::ostream &os, const Society &s);
 };
 
 
--- programming_exercises/pe10_code/main.cpp
+++ programming_exercises/pe10_code/main.cpp
@@ -3,6 +3,14 @@
 
 #include "Society.h"
 
+// Name:
+
+
+// Feel free to edit any of Society.h, Society.cpp and this file in your refactoring
+// In addition to the refactoring tasks:
+// When you are done, this program should be able to easily run for a different number 
+// of cities and you should be able to observe the growth in the cities for a given number of 
+// cycles of growth
 
 int main() {
     Society s;
@@ -10,5 +18,15 @@ int main() {
     s.AddCity(2, 165080, "Fort Collins");
     s.AddCity(3, 18465, "Durango");
     s.AddCity(4, 155, "Ward");
+
+    std::cout << s << std::endl;
+    s.GrowCities();
+    std::cout << s << std::endl;
+    s.GrowCities();
+    std::cout << s << std::endl;
+    s.GrowCities();
+    std::cout << s << std::endl;
+    s.GrowCities();
+    std::cout << s << std::endl;
 }
 
