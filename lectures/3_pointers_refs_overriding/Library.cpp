5b3dc33fd4faa4f6163d674a34114ceda9a27a71
Felix Muzny <muzny@colorado.edu>
2019-09-02 09:55:52 -0600

    lecture 3 exercises

---
+++ lectures/3_pointers_refs_overriding/Library.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Library.h"
+
+/**
+  Zero argument constructor initializes a library with a shelf but no books.
+*/
+Library::Library() {
+  std::vector<Book> tmp;
+  shelf_ = tmp;  // initialize my shelf
+}
+
+/**
+  Prints out each book in the library, one per line to cout
+*/
+void Library::PrintBooks() {
+  for (Book b : shelf_) {
+    std::cout << b.title << " by " << b.author << std::endl;
+  }
+}
