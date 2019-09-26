2516f63b947c3597e761f1091e6a87ec20252b72
Felix Muzny <muzny@colorado.edu>
2019-09-26 09:49:15 -0600

    lecture 10 materials

---
+++ lectures/10_templating_generalizability/exercises.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Names:
+//
+//
+
+
+// 1) implement two functions, AddToValues(std::vector<int> v1, int v2)
+// and AddToValues(std::vector<double> v1, double v2)
+
+
+// 2) Do your AddToValues functions have return values? why/ why not?
+// Answer:
+
+
+int main() {
+    // 3) Instantiate an int vector
+
+
+    // 4) call AddToValues, passing in your int vector and another int.
+
+
+    // 5) compile this file to object code (g++ -std=c++11 -Wall exercises.cpp -o exercises.o),
+    // then run `nm -C exercises.o`. How many versions of the AddToValues function are in the
+    // compiled object code? Copy + paste the relevant lines here:
+    //
+    // To making searching through the output easier, you can pipe the output of nm to grep
+    // `nm -C exercises.o | grep "TextToFindHere"`
+    //
+    // If the -C flag isn't working, you can omit it and still complete the exercise--this flag
+    // makes the output easier to read but isn't strictly necessary
+}
---
+++ lectures/10_templating_generalizability/exercises2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Names:
+//
+//
+
+// 6) implement a templated version of AddToValues, "AddToValues2".
+
+
+int main() {
+    // 7) call AddToValues2, passing in an int vector and another int.
+
+
+    // 8) compile this file to object code (g++ -std=c++11 -Wall exercises2.cpp -o exercises2.o),
+    // then run `nm -C exercises2.o`. How many versions of the AddToValues2 function are in the
+    // compiled object code? Copy + paste the relevant lines here:
+    //
+    //
+
+    // 9) call AddToValues2, passing in a vector of a non-int type and a value that you can add
+    // successfully to the elements
+
+    // 10) compile this file to object code (g++ -std=c++11 -Wall exercises2.cpp -o exercises2.o),
+    // then run `nm -C exercises2.o`. How many versions of the AddToValues2 function are in the
+    // compiled object code? Copy + paste the relevant lines here:
+    //
+    //
+
+    // 11) Experiment to find calls to this function that you would like to work but that do
+    // not yet work. for each function call that doesn't work, comment about what type of 
+    // error is produced.
+
+    // 12) Create a struct that has an int field. Instantiate a vector of these structs.
+    // Instantiate another instance of this struct.
+    
+    // 13) Call AddToValues2 w/ the vector of structs and single struct from #12
+    // Is there an error? If yes, what is it?
+    //
+    // Answer:
+
+    // 14) If there was an error, attempt to fix it.
+}
