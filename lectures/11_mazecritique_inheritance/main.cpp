08c5af6d0dc4ad7d3e41ac4fdf0692de150083b1
Felix Muzny <muzny@colorado.edu>
2019-10-01 10:36:33 -0600

    lecture 11 materials

---
+++ lectures/11_mazecritique_inheritance/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "Animal.h"
+
+// Name(s): 
+
+int main() {
+
+  Reptile ralph("alligator");
+  std::cout << ralph.MakeSound() << std::endl;
+
+  // 1) Instantiate another animal 
+
+  // 2) Call the Fight method of animal
+
+  // 3) Go to the Animal.h file and define some new subclasses of animal
+  // Define one class that inherits from Animal and
+  // one class that inherits from a child class of Animal
+
+  // 4) Instantiate at least one object of each class you defined for #3
+
+  // 5) Have a tournament between the animals that you have instantiated 
+
+  // 6) turn in Animal.h and main.cpp on canvas
+    
+	return 0;
+}
