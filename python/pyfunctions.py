a9a8c27f6d857b143c03492e9011ccd6647d23b1
Felix Muzny <muzny@colorado.edu>
2020-02-13 08:42:47 -0700

    add python resources

---
+++ python/pyfunctions.py
@@ -0,0 +1,22 @@
+
+def add(v1, v2):
+    return v1 + v2
+
+def add_to_values(ls, v2):
+    for i in range(len(ls)):
+        ls[i] += v2 
+        print(ls[i])
+
+# if __name__ == "__main__":
+
+# 	print(add(5,6))
+
+# 	ls_input = [1,12,15]
+# 	val = 10
+# 	add_to_values(ls_input, val)
+
+# 	s = "sample"
+# 	s = "10" + "something"
+# 	s[0] = "s"
+# 	print(s)
+
---
+++ python/pythonresources.md
@@ -0,0 +1,5 @@
+A few resources to get started with python
+---------------
+
+- consult [this cheat sheet](http://www.cogsci.rpi.edu/~destem/igd/python_cheat_sheet.pdf)
+- and/or [these notes that Felix writes for CSCI 1200](https://github.com/muzny/csci1200-notes)
